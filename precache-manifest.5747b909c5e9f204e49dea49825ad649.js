0a62a155d021f6ac537734f16b6020cda0470947
smdeile <smdeile@gmail.com>
2020-05-10 23:56:13 +0300

    Updates

---
+++ precache-manifest.5747b909c5e9f204e49dea49825ad649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921a2d182761ee64a437eb0576aa8554",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "fbe0857cfa90f36ce261",
+    "url": "/goit-react-hw-04-movies/static/css/main.3b172962.chunk.css"
+  },
+  {
+    "revision": "ca5243bafc40fd04dc92",
+    "url": "/goit-react-hw-04-movies/static/js/2.76b7332c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.76b7332c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe0857cfa90f36ce261",
+    "url": "/goit-react-hw-04-movies/static/js/main.47aaf63f.chunk.js"
+  },
+  {
+    "revision": "e34e4fd2a07b25362320",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.aa12acaf.js"
+  }
+]);
